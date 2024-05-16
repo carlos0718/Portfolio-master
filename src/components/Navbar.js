c8355c9a86fecd9e8eb7ad55829397adde99ef2c
c.sipiran07@gmail.com <carlos0718>
2024-05-16 18:17:32 -0300

    hide option resume

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
-import { CgGitFork } from "react-icons/cg";
-import { AiFillStar, AiOutlineHome, AiOutlineFundProjectionScreen, AiOutlineUser } from "react-icons/ai";
+import {Link} from "react-router-dom";
+import {CgGitFork} from "react-icons/cg";
+import {AiFillStar, AiOutlineHome, AiOutlineFundProjectionScreen, AiOutlineUser} from "react-icons/ai";
 
-import { CgFileDocument } from "react-icons/cg";
+import {CgFileDocument} from "react-icons/cg";
 
 function NavBar() {
 	const [expand, updateExpanded] = useState(false);
@@ -40,34 +40,30 @@ function NavBar() {
 					<Nav className='ms-auto' defaultActiveKey='#home'>
 						<Nav.Item>
 							<Nav.Link as={Link} to='/' onClick={() => updateExpanded(false)}>
-								<AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+								<AiOutlineHome style={{marginBottom: "2px"}} /> Home
 							</Nav.Link>
 						</Nav.Item>
 
 						<Nav.Item>
 							<Nav.Link as={Link} to='/about' onClick={() => updateExpanded(false)}>
-								<AiOutlineUser style={{ marginBottom: "2px" }} /> About
+								<AiOutlineUser style={{marginBottom: "2px"}} /> About
 							</Nav.Link>
 						</Nav.Item>
 
-{/* 						<Nav.Item>
+						{/* 						<Nav.Item>
 							<Nav.Link as={Link} to='/project' onClick={() => updateExpanded(false)}>
 								<AiOutlineFundProjectionScreen style={{ marginBottom: "2px" }} /> Projects
 							</Nav.Link>
 						</Nav.Item> */}
 
-						<Nav.Item>
+						{/* <Nav.Item>
 							<Nav.Link as={Link} to='/resume' onClick={() => updateExpanded(false)}>
 								<CgFileDocument style={{ marginBottom: "2px" }} /> Resume
 							</Nav.Link>
-						</Nav.Item>
+						</Nav.Item> */}
 						<Nav.Item className='fork-btn'>
-							<Button
-								href='https://github.com/carlos0718/Portfolio'
-								target='_blank'
-								className='fork-btn-inner'
-							>
-								<CgGitFork style={{ fontSize: "1.2em" }} /> <AiFillStar style={{ fontSize: "1.1em" }} />
+							<Button href='https://github.com/carlos0718/Portfolio' target='_blank' className='fork-btn-inner'>
+								<CgGitFork style={{fontSize: "1.2em"}} /> <AiFillStar style={{fontSize: "1.1em"}} />
 							</Button>
 						</Nav.Item>
 					</Nav>
